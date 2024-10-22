7f9924a8e380600826544ff3737a4766e8eb6e67
Martin H. Normark <m@martinnormark.com>
2024-10-22 23:36:25 +0200

    chore: Improve tests of encode-decode

--- src/data-wrangler.test.ts
+++ src/data-wrangler.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "bun:test";
 import { encode, decode } from "./data-wrangler";
 
-test("Encoding and decoding produce the same result", () => {
+test("Encoding and decoding produce the same result with small numbers", () => {
   const data = Array.from({ length: 20 }, () => Math.floor(Math.random() * 10));
 
   const encoded = encode(data, Math.min(...data), Math.max(...data));
@@ -9,5 +9,18 @@ test("Encoding and decoding produce the same result", () => {
 
   decoded.forEach((d, i) => {
     expect(d).toBeCloseTo(data[i]);
+  });
+});
+
+test("Encoding and decoding produce the same result with large numbers", () => {
+  const data = Array.from({ length: 20 }, () =>
+    Math.floor(Math.random() * 1000)
+  );
+
+  const encoded = encode(data, Math.min(...data), Math.max(...data));
+  const decoded = decode(encoded, Math.min(...data), Math.max(...data));
+
+  decoded.forEach((d, i) => {
+    expect(d).toBeCloseTo(data[i], -1.5);
   });
 });
